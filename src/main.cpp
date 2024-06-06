663cb3c5331762593ad99b85ee3879e079758b42
CodeMyGame1 <pranesh@yosanai.com>
2024-06-05 19:36:34 -0700

    add basic, working tuningCLI() function! also adds functionality to toggle between driver control/tuning

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,22 @@
  * IMPORTS:
 */
 #include <cmath>
+#include <algorithm>
+#include <string>
 
 #include "main.h"
 
 #include "lemlib/api.hpp"
+
+/**
+ * CONSTS:
+*/
+
+// enum pid_consts {
+// 	P = 0
+// 	, I = 1
+// 	, D = 2
+// };
 
 /**
  * CONFIG VARS:
@@ -76,10 +88,6 @@ pros::Motor_Group right_motors({right_front, right_middle, right_back});
 pros::Rotation h_track_wheel_rot(18);
 
 pros::ADIEncoder v_track_wheel_adi('C', 'D');
-
-pros::ADIEncoder kp_tuner('A', 'B', true);
-
-pros::ADIEncoder kd_tuner('G', 'H');
 
 // inertial sensor definitions
 /** TODO: find port of inertial sensor! */
@@ -176,13 +184,6 @@ void screenTaskFunc(void* chassis) {
 	lemlib::Chassis* myChassis = (lemlib::Chassis *)(chassis);
 
 	while (true) {
-		// only enables functionality of physical PID tuner
-		// if `tuningPID` is set to `true`
-		if (tuningPID) {
-			kp_target = round(kp_tuner.get_value() / 100);
-			kd_target = round(kd_tuner.get_value() / 100);
-		}
-
 		char runningPIDTestBufferString[20];
 
 		// the space in the format string is important... don't delete it!
@@ -236,6 +237,194 @@ void screenTaskFunc(void* chassis) {
 		);
 		
 		pros::delay(20);
+	}
+}
+
+std::vector<std::string> split(const std::string& _input,
+                               const std::string& delimiter) {
+	std::vector<std::string> tokens;
+
+	std::string source = _input;
+
+	size_t pos = 0;
+
+	while ((pos = source.find(delimiter)) != std::string::npos) {
+		tokens.push_back(source.substr(0, pos));
+
+		source.erase(0, pos + delimiter.length());
+	}
+
+	// returns last entry AFTER delimiter
+	tokens.push_back(source);
+
+	return tokens;
+}
+
+void makeLowerCase(std::string& str) {
+	std::transform(
+		str.begin()		// passes in the
+		, str.end()		// full string to be transformed
+
+		, str.begin()	// section of string to start inserting
+						// transformed string into
+		
+		// basically lambda function that returns lowercase version of
+		// each character in the string
+		, [](unsigned char c) { return std::tolower(c); }
+	);
+}
+
+/**
+ * inspired by code from: https://github.com/meisZWFLZ/OverUnder781X
+*/
+void tuningCLI() {
+	lemlib::PID* pid = &(runningLinearPIDTest ? chassis.lateralPID
+											  : chassis.angularPID);
+
+	while (tuningPID) {
+		try {
+			// informs user they can start typing command
+			std::cout << "enter command> ";
+
+			// fetches command (WAITS UNTIL NEWLINE)
+			// and formats it to lowercase
+			std::string input;
+			getline(std::cin, input);
+			makeLowerCase(input);
+
+			auto params = split(input, " ");
+
+			std::string command = params.at(0);
+
+			if (command == "s" || command == "set") {
+				if (params.size() < 3) {
+					std::cout << "not enough arguments to process request (need 3!)..." << std::endl;
+					continue;
+				}
+
+				std::string whichGain = params.at(1);
+				std::string valueToSetStr = params.at(2);
+
+				float *constToSet = nullptr;
+
+
+				if (whichGain == "p") {
+					constToSet = &(pid->kP);
+				
+				} else if (whichGain == "i") {
+					constToSet = &(pid->kI);
+				
+				} else if (whichGain == "d") {
+					constToSet = &(pid->kD);
+				
+				} else {
+					std::cout << " | INVALID gain to set!" << std::endl;
+					continue;
+				}
+
+				// stores old value of the PID constant, to inform the user later
+				float oldValue = (*constToSet);
+
+				// initializes `valueToSet`
+				float valueToSet = -1;
+
+				/**
+				 * if the user types in a certain string as the second parameter (that is, NOT a float),
+				 * fetches the PID constant they want to change, and adds or subtracts 1 or 2, accordingly
+				 * to get its new value
+				*/
+				if (valueToSetStr == "+1") {
+					valueToSet = (*constToSet) + 1;
+				} else if (valueToSetStr == "-1") {
+					valueToSet = (*constToSet) - 1;
+				
+				} else if (valueToSetStr == "+2") {
+					valueToSet = (*constToSet) + 2;
+				} else if (valueToSetStr == "-2") {
+					valueToSet = (*constToSet) - 2;
+				
+				} else {
+					try {
+						// tries to convert user's desired gain value to a float
+						valueToSet = std::stof(valueToSetStr);
+					} catch (const std::invalid_argument &e) {
+						// we were scammed! the user didn't pass in a float!
+						std::cout << " | Gain value not a valid float!" << std::endl;
+						continue;
+					}
+				}
+
+				// sets the new value of the PID constant!
+				(*constToSet) = valueToSet;
+
+				printf(" | successfully changed gain value! old value: %f, new value %f!\n", oldValue, *constToSet);
+
+			} else if (command == "g" || command == "get") {
+				if (params.size() < 2) {
+					std::cout << "not enough arguments to process request (need 2!)..." << std::endl;
+					continue;
+				}
+
+				std::string whatInfo = params.at(2);
+
+				if (whatInfo == "mode") {
+					std::cout << " | currently tuning " << (runningLinearPIDTest ? "LINEAR" : "ANGULAR") << " PID" << std::endl;
+				
+				} else if (whatInfo == "p") {
+					std::cout << " | kP: " << pid->kP << std::endl;
+				
+				} else if (whatInfo == "i") {
+					std::cout << " | kI: " << pid->kI << std::endl;
+				
+				} else if (whatInfo == "d") {
+					std::cout << " | kD: " << pid->kD << std::endl;
+				
+				} else {
+					std::cout << " | INVALID gain to fetch info for!" << std::endl;
+				}
+			
+			} else if (command == "turn-left") { // returns robot to original position IF tuning angular PID
+				if (!runningLinearPIDTest) {
+					chassis.setPose(0, 0, 0);
+
+					chassis.turnToHeading(-90, 1500, {}, false);
+				}
+
+			} else if (command == "run" || command == "r") {
+				runningPIDTest = true;
+
+				// resets position before runs, in case test auton is being run multiple times
+				chassis.setPose(0, 0, 0);
+
+				switch (runningLinearPIDTest) {
+					// running linear PID test
+					case true:
+						chassis.moveToPoint(0, -24, 3000, linearPIDTestMoveToPointParams, false);
+						
+						break;
+					default:
+						chassis.turnToHeading(90, 1500, {}, false);
+
+						pros::delay(1000);
+				}
+			
+			} else if (command == "stop") {
+				chassis.cancelAllMotions();
+			
+			} else if (command == "exit") {
+				std::cout << " | switching to driver control..." << std::endl << std::endl << "---" << std::endl << std::endl;
+
+				chassis.cancelAllMotions();
+				tuningPID = false;
+
+				break;
+			
+			} else {
+				std::cout << "| not a valid command..." << std::endl;
+			}
+		} catch (std::exception e) {
+			std::cout << " | something went wrong: " << e.what() << std::endl;
+		}
 	}
 }
 
@@ -311,12 +500,12 @@ void opcontrol() {
 		if (!tuningPID) {
 			/* normal driver control */
 
-			// allows for toggling between tuning PID and driver control
-			bool B_new_press = controller.get_digital_new_press(DIGITAL_B);
+			bool X_pressed = controller.get_digital(DIGITAL_X);
+			bool A_pressed = controller.get_digital(DIGITAL_A);
 
-			printf("Is B pressed (not tuning PID mode): %i\n", B_new_press);
-
-			if (B_new_press) {
+			// if both the X and A buttons are pressed
+			if (X_pressed && A_pressed) {
+				printf("X and A were both pressed, transferring to tuning PID mode...\n\n---\n\n");
 				tuningPID = true;
 			}
 
@@ -325,56 +514,7 @@ void opcontrol() {
 		} else {
 			/* tuning PID! wee! */
 
-			// toggle between tuning PID and driver control
-			bool B_new_press = controller.get_digital_new_press(DIGITAL_B);
-
-			printf("Is B pressed (tuning PID mode): %i\n", B_new_press);
-
-			if (B_new_press) {
-				tuningPID = false;
-			}
-
-			// checks if the `A` button has been NEWLY pressed
-			// (i.e. does not fire multiple times if the `A`
-			// button is HELD)
-			bool A_new_press = controller.get_digital_new_press(DIGITAL_A);
-
-			// if A is pressed, and the test auton is not currently running
-			if (A_new_press && !runningPIDTest) {
-				runningPIDTest = true;
-
-				// makes sure PID constants are not negative!
-				kp_target = abs(kp_target);
-				kd_target = abs(kd_target);
-
-				// based on whether linear/angular
-				// PID is being tuned, set the corresponding
-				// kP, kD values and run the corresponding
-				// test auton
-				if (runningLinearPIDTest) {
-					if (usingPhysicalPIDTuner) {
-						chassis.lateralPID.kP = kp_target;
-						chassis.lateralPID.kD = kd_target;
-					}
-
-					// resets position before runs, in case test auton is being run multiple times
-					chassis.setPose(0, 0, 0);
-
-					chassis.moveToPoint(0, -24, 3000, linearPIDTestMoveToPointParams, false);
-				} else {
-					if (usingPhysicalPIDTuner) {
-						chassis.angularPID.kP = kp_target;
-						chassis.angularPID.kD = kd_target;
-					}
-
-					// resets heading before runs
-					chassis.setPose(0, 0, 0);
-
-					chassis.turnToHeading(90, 1500, {}, false);
-				}
-
-				runningPIDTest = false;
-			}
+			tuningCLI();
 		}
 
 		// delay to save system resources
