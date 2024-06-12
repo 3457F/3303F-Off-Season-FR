9c0ed5c365f9f42cb9e835114b4b9884fc332dbc
CodeMyGame1 <pranesh@yosanai.com>
2024-06-11 17:59:59 -0700

    upgraded to newer version of PROS/LemLib as per #9

--- include/lemlib/pid.hpp
+++ include/lemlib/pid.hpp
@@ -60,13 +60,12 @@ class PID {
          * @endcode
          */
         void reset();
-
-        // gains
-        float kP;
-        float kI;
-        float kD;
-
     protected:
+        // gains
+        const float kP;
+        const float kI;
+        const float kD;
+
         // optimizations
         const float windupRange;
         const bool signFlipReset;
